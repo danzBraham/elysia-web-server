a3aa9da100a66d653fb979f26a56beed22a3b493
Zidan Abraham <zidangit07@gmail.com>
2023-12-26 16:00:26 +0800

    feat: basic handler & context

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,22 @@ import { Elysia } from 'elysia';
 
 const app = new Elysia();
 
-app.get('/', () => 'Home Page Elysia');
+app.state('version', 1);
+app.state('counter', 0);
+app.decorate('logger', console);
+
+app.get('/', ({ set, store, logger }) => {
+  set.status = 'OK';
+  set.headers['Content-Type'] = 'application/json';
+  set.headers['X-Powered-By'] = 'Elysia';
+
+  logger.log(`${store.counter}. Hello from log`);
+  return {
+    status: 'success',
+    message: 'Hello World!',
+    counter: `${store.counter++}x refreshed`,
+  };
+});
 app.route('M-SEARCH', '/', () => 'connect');
 app.all('/', () => 'Hello');
 
@@ -17,14 +32,16 @@ app.get(
   ({ params: { category, slug } }) => `Blog category: ${category}, blog: ${slug}`
 );
 
+app.get('/penguin', Bun.file(import.meta.resolveSync('./penguin-coding.gif')));
+
 app.onError(({ code }) => {
   if (code === 'NOT_FOUND') return 'Route not found :(';
 });
 
 app.listen(3000);
 
-app.handle(new Request('http://localhost:3000/')).then(console.log);
-app.handle(new Request('http://localhost:3000/about')).then(console.log);
-app.handle(new Request('http://localhost:3000/blogs')).then(console.log);
+// app.handle(new Request('http://localhost:3000/')).then(console.log);
+// app.handle(new Request('http://localhost:3000/about')).then(console.log);
+// app.handle(new Request('http://localhost:3000/blogs')).then(console.log);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
