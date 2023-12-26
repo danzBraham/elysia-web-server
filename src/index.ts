53c9d4027ed390d2a070661b8a5fe67bea07d09a
Zidan Abraham <zidangit07@gmail.com>
2023-12-26 12:25:25 +0800

    feat: basic routing

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,20 @@ import { Elysia } from 'elysia';
 
 const app = new Elysia();
 
-app.get('/', () => 'Hello Elysia');
+app.get('/', () => 'Home Page Elysia');
+app.route('M-SEARCH', '/', () => 'connect');
+app.all('/', () => 'Hello');
 app.get('/about', () => 'About Elysia');
 app.get('/blogs', () => 'This Elysia Blog');
+app.post('/blogs', () => 'Posting a Blog');
+app.onError(({ code }) => {
+  if (code === 'NOT_FOUND') return 'Route not found :(';
+});
 
 app.listen(3000);
+
+app.handle(new Request('http://localhost:3000/')).then(console.log);
+app.handle(new Request('http://localhost:3000/about')).then(console.log);
+app.handle(new Request('http://localhost:3000/blogs')).then(console.log);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
