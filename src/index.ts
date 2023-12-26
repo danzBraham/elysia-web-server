50b7fbb68ff5d598398057d4061b0e2bce1a19c5
Zidan Abraham <zidangit07@gmail.com>
2023-12-26 12:42:28 +0800

    feat: basic path

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,18 @@ const app = new Elysia();
 app.get('/', () => 'Home Page Elysia');
 app.route('M-SEARCH', '/', () => 'connect');
 app.all('/', () => 'Hello');
+
 app.get('/about', () => 'About Elysia');
+app.get('/about/*', ({ params }) => `wildcard path: ${params['*']}`);
+
 app.get('/blogs', () => 'This Elysia Blog');
 app.post('/blogs', () => 'Posting a Blog');
+app.get('/blogs/:category', ({ params: { category } }) => `Blog category: ${category}`);
+app.get(
+  '/blogs/:category/:slug',
+  ({ params: { category, slug } }) => `Blog category: ${category}, blog: ${slug}`
+);
+
 app.onError(({ code }) => {
   if (code === 'NOT_FOUND') return 'Route not found :(';
 });
